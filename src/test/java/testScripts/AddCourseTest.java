5c6b5eea9db3d40c97a5305d5ad52af38386bd0c
aravi <aravi@DESKTOP-ENSOR03>
2024-02-09 13:28:19 +0530

    Framework A2

--- src/test/java/testScripts/AddCourseTest.java
+++ src/test/java/testScripts/AddCourseTest.java
@@ -9,7 +9,8 @@ import genericUtilities.BaseClass;
 import genericUtilities.IconstantPath;
 
 public class AddCourseTest extends BaseClass {
-//This method is used to add course
+//This method is used to add course 
+	//This is used
 	@Test
 	
 	public void addCourseTest() {
